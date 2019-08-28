775b00cc9dfa0ae247aa404d4d886574f07ea96e
Aleix Rius <aleixrius19@gmail.com>
2019-08-28 14:36:56 +0200

    Add files via upload

--- TFG_ComputeShader/TFG_ComputeShader/TriangleFShader.glsl
+++ TFG_ComputeShader/TFG_ComputeShader/TriangleFShader.glsl
@@ -1,9 +1,13 @@
 #version 400 core
 
 in vec3 normal;
+in vec4 pos;
 out vec4 FragColor;
 
 void main()
 {
-	FragColor = vec4(normal,1.0);
+	vec3 lightSource = vec3(-150,40,0); 
+	float result = dot(lightSource, normal);
+	vec3 c = clamp(pos.rgb,vec3(0.0),vec3(1.0));
+	FragColor = vec4(c,1.0) * result;
 }
--- TFG_ComputeShader/TFG_ComputeShader/TriangleVShader.glsl
+++ TFG_ComputeShader/TFG_ComputeShader/TriangleVShader.glsl
@@ -7,14 +7,16 @@ uniform mat4 view;
 uniform mat4 model; 
 uniform mat4 projection;
 uniform mat4 transform;
+uniform mat4 root;
 
 out vec3 normal; 
-
+out vec4 pos; 
 void main()
 {
 	mat4 MVP = projection * view * model;
 	//MVP = mat4(1.0);
-	vec4 p = transform * vec4(aPos,1.0);
+	vec4 p = root *transform * vec4(aPos,1.0);
 	gl_Position =  MVP * p;//Posicio amb les transformacions de camera
 	normal = aNormal;
+	pos = p;
 }
